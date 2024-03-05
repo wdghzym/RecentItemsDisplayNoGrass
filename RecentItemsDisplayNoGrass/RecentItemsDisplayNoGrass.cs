c57a7a7c27d2cadea929bff247df19cb193e7b8c
wdghzym <67120670+wdghzym@users.noreply.github.com>
2024-03-05 09:18:15 +0800

    1.0

---
+++ RecentItemsDisplayNoGrass/RecentItemsDisplayNoGrass.cs
@@ -0,0 +1,33 @@
+﻿using Modding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.IO.LowLevel.Unsafe;
+
+namespace RecentItemsDisplayNoGrass
+{
+    public class RecentItemsDisplayNoGrass:Mod
+    {
+        public RecentItemsDisplayNoGrass() { }
+        public override string GetVersion()
+        {
+            return GetType().Assembly.GetName().Version.ToString();
+        }
+        public override int LoadPriority() => 5;
+        public override void Initialize()
+        {
+
+            base.Initialize();
+            RecentItemsDisplay.Events.ModifyDisplayItem += Events_ModifyDisplayItem;
+        }
+
+        private void Events_ModifyDisplayItem(RecentItemsDisplay.ItemDisplayArgs obj)
+        {
+            if (obj != null)
+                if (obj.DisplayName=="Grass")
+                    obj.IgnoreItem = true;
+        }
+    }
+}
